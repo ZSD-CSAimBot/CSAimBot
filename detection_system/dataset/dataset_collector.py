4f0e0bab9d12c2ced9758b50b18354889e7b95d1
andrz <andrzej200407@wp.pl>
2026-03-15 22:33:08 +0100

    Update dataset.

--- detection_system/dataset/dataset_collector.py
+++ detection_system/dataset/dataset_collector.py
@@ -5,17 +5,19 @@ import bettercam
 import win32api
 
 #Version for 1080p monitor
-'''
+
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 FOV_WIDTH = 1280
 FOV_HEIGHT = 736
-'''
+
 #Version for 2k monitor
+'''
 SCREEN_WIDTH = 2560
 SCREEN_HEIGHT = 1440
 FOV_WIDTH = 1728
 FOV_HEIGHT = 992
+'''
 
 left = (SCREEN_WIDTH // 2) - (FOV_WIDTH // 2)
 top = (SCREEN_HEIGHT // 2) - (FOV_HEIGHT // 2)
@@ -27,7 +29,7 @@ REGION = (left, top, right, bottom)
 LBUTTON = 0x01
 Y = 0x59
 
-OUTPUT_DIR = "./detection_system/dataset/images"
+OUTPUT_DIR = "./detection_system/dataset/images_andrzej"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 def main():
@@ -42,7 +44,7 @@ def main():
                 filepath = os.path.join(OUTPUT_DIR, filename)
 
                 #Only for 2k monitor - for 1080p frame is already 1280x736
-                frame = cv2.resize(frame, (1280, 736))
+                #frame = cv2.resize(frame, (1280, 736))
 
                 cv2.imwrite(filepath, frame)
                 print(f"Screenshot: {filename}")
