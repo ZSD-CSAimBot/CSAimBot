309c2f3f0c56861efa8b63921600a6b3f9a983b6
Filip Pietrzak <filip.j.pietrzak@gmail.com>
2026-04-29 14:13:47 +0200

    Revert "Reapply "Merge pull request #63 from ZSD-CSAimBot/assembly_and_app""
    This reverts commit 22e739fd47b86095a2de89f0a6d32a86d0958231.

--- application/app.py
+++ application/app.py
@@ -9,23 +9,20 @@ from gui_design.gui import GUI
 if __name__ == "__main__":
     mp.freeze_support()
 
-    """ NA CZAS TESTÓW - USUNĄĆ PO USTABILIZOWANIU SIĘ PROJEKTU
     MODEL_PATH = os.path.abspath('application/detection_system/yolo/trained_model.engine')
     if not os.path.exists(MODEL_PATH):
         print("Trwa eksport modelu do TensorRT. Proszę czekać...")
         export_model_to_trt()
-    """
+
     gui_vision_conn, vision_worker_conn = mp.Pipe()
     gui_comms_conn, comms_worker_conn = mp.Pipe()
     # Uruchomienie procesu odpowiedzialnego za logikę (AimBot / Robot)
-    """ NA CZAS TESTÓW - USUNĄĆ PO USTABILIZOWANIU SIĘ PROJEKTU
     vision_process = mp.Process(
         target=vision_worker,
         args=(vision_worker_conn, MODEL_PATH, 60),
         daemon=True
     )
     vision_process.start()
-    """
     # Uruchomienie procesu odpowiedzialnego za komunikację
     comms_process = mp.Process(
         target=comms_worker,
@@ -38,5 +35,5 @@ if __name__ == "__main__":
     app.run()
 
     # Bezpieczne zamknięcie
-    #vision_process.join()
+    vision_process.join()
     comms_process.join()
